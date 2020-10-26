bad8cfb4fa6539478d0832c7aa7c8284fc38d0ce
Alikhan <autocardag@gmail.com>
2020-10-26 15:20:21 +0200

    Update Commandgc.java

--- src/Commandgc.java
+++ src/Commandgc.java
@@ -45,7 +45,7 @@ public class Commandgc extends EssentialsCommand {
         }));
         List < World > worlds = server.getWorlds();
         for (World w: worlds) {
-            if (Integer.valueOf((w.getLoadedChunks()).length) == 0) //IF world has 0 LoadedChunks - it will skipped from for
+            if (Integer.valueOf((w.getLoadedChunks()).length) == 0) //IF world has 0 LoadedChunks - it will be skipped from for
                 continue;
             String worldType = "World";
             switch (w.getEnvironment()) {
